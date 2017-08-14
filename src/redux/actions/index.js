e54c15e09e5150c6957062c4e32bcf01b0d0799d
rising-talent <li.yang.react726@gmail.com>
2017-08-15 01:07:06 +0800

    bug fix ( 8.14 )

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,10 +1,10 @@
-import * as mainctions from './main'
+import * as mainActions from './main'
 import * as checkActions from './check'
 import * as firebaseActions from './firebase'
 import * as storageActions from './storage'
 
 export const ActionCreators = Object.assign({},
-  mainctions,
+  mainActions,
   checkActions,
   firebaseActions,
   storageActions
